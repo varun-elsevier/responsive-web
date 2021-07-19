6ff036d50088f976d364c91c8d3b85b5551f4a37
Varun Sharma <v.sharma@elsevier.com>
2021-07-19 16:34:58 +0530

    reactive dependencies

--- src/main/java/com/example/review/PlainUserController.java
+++ src/main/java/com/example/review/PlainUserController.java
@@ -1,50 +1,89 @@
 package com.example.review;
 
-import com.example.review.Review;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.core.publisher.Mono;
+import reactor.util.context.Context;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.function.LongConsumer;
 
 @RestController
 @RequestMapping("user")
 public class PlainUserController {
-    final SseEmitter sseEmitter = new SseEmitter(TimeUnit.MINUTES.toMillis(5));
 
-    final List<Review> allReviews = Stream.of(1, 2, 3, 4, 5)
+    static class Sink implements FluxSink<Review> {
+
+
+        @Override
+        public FluxSink<Review> next(Review review) {
+            return null;
+        }
+
+        @Override
+        public void complete() {
+
+        }
+
+        @Override
+        public void error(Throwable e) {
+
+        }
+
+        @Override
+        public Context currentContext() {
+            return null;
+        }
+
+        @Override
+        public long requestedFromDownstream() {
+            return 0;
+        }
+
+        @Override
+        public boolean isCancelled() {
+            return false;
+        }
+
+        @Override
+        public FluxSink<Review> onRequest(LongConsumer consumer) {
+            return null;
+        }
+
+        @Override
+        public FluxSink<Review> onCancel(Disposable d) {
+            return null;
+        }
+
+        @Override
+        public FluxSink<Review> onDispose(Disposable d) {
+            return null;
+        }
+    }
+
+    final Flux<Review> allReviews = Flux.just(1, 2, 3, 4, 5)
             .map(id -> new Review(id, "test"))
-            .collect(Collectors.toList());
-
+            .share();
     @GetMapping
-    public List<Review> getAllReviews() {
+    public Flux<Review> getAllReviews() {
         return allReviews;
     }
 
     @GetMapping("/{id}")
-    public Optional<Review> getOneReview(@PathVariable Integer id) {
-        Optional<Review> reviewOptional = allReviews.stream().filter(review -> review.id == id)
-                .findFirst();
-        reviewOptional.ifPresent(review -> {
-            try {
-                sseEmitter.send(review);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-        return reviewOptional;
+    public Mono<Review> getOneReview(@PathVariable Integer id) {
+        return allReviews.filter(review -> review.id == id)
+                .next();
     }
 
     @GetMapping("/locked")
-    public SseEmitter getLocked() {
-        return sseEmitter;
+    public Flux<Review> getLocked() {
+        Flux.create(fluxSink -> fluxSink.)
+        return Flux.just();
     }
+
+
 }

62cf96bf8c5e44beda9167a448e0b37950f80dcc
Varun Sharma <v.sharma@elsevier.com>
2021-07-21 11:20:12 +0530

    reactive controller

--- src/main/java/com/example/review/PlainUserController.java
+++ src/main/java/com/example/review/PlainUserController.java
@@ -1,73 +1,24 @@
 package com.example.review;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.Mono;
-import reactor.util.context.Context;
-
-import java.util.function.LongConsumer;
+import reactor.core.publisher.Sinks;
 
 @RestController
 @RequestMapping("user")
 public class PlainUserController {
 
-    static class Sink implements FluxSink<Review> {
-
-
-        @Override
-        public FluxSink<Review> next(Review review) {
-            return null;
-        }
-
-        @Override
-        public void complete() {
-
-        }
-
-        @Override
-        public void error(Throwable e) {
-
-        }
-
-        @Override
-        public Context currentContext() {
-            return null;
-        }
-
-        @Override
-        public long requestedFromDownstream() {
-            return 0;
-        }
-
-        @Override
-        public boolean isCancelled() {
-            return false;
-        }
-
-        @Override
-        public FluxSink<Review> onRequest(LongConsumer consumer) {
-            return null;
-        }
-
-        @Override
-        public FluxSink<Review> onCancel(Disposable d) {
-            return null;
-        }
-
-        @Override
-        public FluxSink<Review> onDispose(Disposable d) {
-            return null;
-        }
-    }
+    final Sinks.Many<Review> sink = Sinks.many().replay().all();
 
     final Flux<Review> allReviews = Flux.just(1, 2, 3, 4, 5)
             .map(id -> new Review(id, "test"))
             .share();
+
     @GetMapping
     public Flux<Review> getAllReviews() {
         return allReviews;
@@ -76,13 +27,16 @@ public class PlainUserController {
     @GetMapping("/{id}")
     public Mono<Review> getOneReview(@PathVariable Integer id) {
         return allReviews.filter(review -> review.id == id)
-                .next();
+                .next()
+                .doOnNext(review ->
+                        sink.emitNext(review, Sinks.EmitFailureHandler.FAIL_FAST)
+                );
     }
 
-    @GetMapping("/locked")
+    @GetMapping(value = "/locked", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Review> getLocked() {
-        Flux.create(fluxSink -> fluxSink.)
-        return Flux.just();
+
+        return sink.asFlux();
     }
 
 
